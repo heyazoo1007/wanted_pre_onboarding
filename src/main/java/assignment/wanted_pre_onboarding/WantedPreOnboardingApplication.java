22d163a9c0a185fe7a6b74d888e0818f3b8c0c57
heyazoo1007 <heyazoo1007@gmail.com>
2022-08-23 14:06:17 +0900

    first commit

---
+++ src/main/java/assignment/wanted_pre_onboarding/WantedPreOnboardingApplication.java
@@ -0,0 +1,13 @@
+package assignment.wanted_pre_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedPreOnboardingApplication.class, args);
+	}
+
+}
