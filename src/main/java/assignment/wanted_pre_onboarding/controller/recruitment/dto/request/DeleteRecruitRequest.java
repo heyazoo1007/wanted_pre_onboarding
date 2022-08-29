9def9a3df2bc9e8cce380dd4e87ce4a9ab1d1a0a
heyazoo1007 <heyazoo1007@gmail.com>
2022-08-29 18:03:55 +0900

    [FIX] 코드 수정

--- src/main/java/assignment/wanted_pre_onboarding/controller/recruitment/dto/request/DeleteRecruitRequest.java
+++ src/main/java/assignment/wanted_pre_onboarding/controller/recruitment/dto/request/DeleteRecruitRequest.java
@@ -1,9 +1,10 @@
 package assignment.wanted_pre_onboarding.controller.recruitment.dto.request;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DeleteRecruitRequest {
     private Long recruitId;
 
